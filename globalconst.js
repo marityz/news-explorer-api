1ae04ceb3ac800ba444b473b91358af0f38a0727
Marina Avinkina <marityz@gmail.com>
2020-09-23 15:39:23 +0300

    Поправлен адрес базы

--- globalconst.js
+++ globalconst.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 const {
-  NODE_ENV, JWT_SECRET, DATABASE, PORT,
+  NODE_ENV, JWT_SECRET, PORT,
 } = process.env;
 
 module.exports.PORT = PORT || 3000;
 
-module.exports.DATABASE = NODE_ENV === 'production' ? DATABASE : 'mongodb://localhost:27017/data';
+module.exports.DATABASE = 'mongodb://localhost:27017/data';
 
 module.exports.JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
