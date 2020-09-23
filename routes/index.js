140d8ab554cce3e9e89b4580023e3e282b4c4a9a
Marina Avinkina <marityz@gmail.com>
2020-09-23 16:30:14 +0300

    Модуль маршрутизации авторизации вынесен в отдельный файл

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,11 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const { createUser, login } = require('../controllers/users');
+const authRouter = require('./authorization');
 const usersRoutes = require('./users');
 const articlesRouter = require('./articles');
 const { auth } = require('../middlewares/auth');
 const { nonExistentPaths } = require('../controllers/non-existent-paths');
 
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
-  }),
-}), createUser);
-
+router.use('/', authRouter);
 router.use('/users', auth, usersRoutes);
 router.use('/articles', auth, articlesRouter);
 router.use('/*', nonExistentPaths);
