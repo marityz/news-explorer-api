05619c019adb5b600b5e5732d1a62908cd27c59f
Marina Avinkina <marityz@gmail.com>
2020-09-23 16:05:18 +0300

    Поправлена маршрутизация

--- globalconst.js
+++ globalconst.js
@@ -6,6 +6,6 @@ const {
 
 module.exports.PORT = PORT || 3000;
 
-module.exports.DATABASE = 'mongodb://127.0.0.1:27017/data';
+module.exports.DATABASE = 'mongodb://localhost:27017/data';
 
 module.exports.JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
