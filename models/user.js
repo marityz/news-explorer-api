038e3ea016be98e816c282cfe87e9434135a5819
Marina Avinkina <marityz@gmail.com>
2020-09-23 19:35:45 +0300

    убраны // eslint-disable-next-line func-names

--- models/user.js
+++ models/user.js
@@ -24,8 +24,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// eslint-disable-next-line func-names
-userSchema.statics.findUserByCredentials = function (email, password) {
+function findUserByCredentials(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
@@ -41,6 +40,8 @@ userSchema.statics.findUserByCredentials = function (email, password) {
           return user;
         });
     });
-};
+}
+
+userSchema.statics.findUserByCredentials = findUserByCredentials;
 
 module.exports = mongoose.model('user', userSchema);
